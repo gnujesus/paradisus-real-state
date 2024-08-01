be3a02c2d51e903ba3aa07ec6895be30d4eceaf6
IPolonio <Polonio.isaac@gmail.com>
2024-08-01 15:50:26 -0400

    Changes

--- RealStateApp.Core.Application/Interfaces/Services/IPropertyService.cs
+++ RealStateApp.Core.Application/Interfaces/Services/IPropertyService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using RealStateApp.Core.Application.ViewModels.PropertyModels;
+using RealStateApp.Core.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,15 @@ using System.Threading.Tasks;
 
 namespace RealStateApp.Core.Application.Interfaces.Services
 {
-    public interface IPropertyService
+    public interface IPropertyService : IGenericService<SavePropertyViewModel,PropertyViewModel, PropertyViewModel>
     {
+
+        Task<IEnumerable<Property>> GetAllPropertiesWithFavorites(string userId);
+        Task<Property> GetPropertyByCode(string code);
+        Task<IEnumerable<Property>> GetPropertiesByAgentId(string agentId);
+        Task<int> GetTotalPropertiesCount();
+        Task<IEnumerable<Property>> FilterProperties(string typePropertyId = null, decimal? minPrice = null, decimal? maxPrice = null, int? rooms = null, int? bathrooms = null);
+        new Task<List<Property>> GetAll();
+
     }
 }
