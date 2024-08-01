a0adc29a7db749aae190eb1ebbcbfc1cfc9c8315
IPolonio <Polonio.isaac@gmail.com>
2024-08-01 17:09:31 -0400

    Varon le toca

---
+++ RealStateApp.Core.Application/Services/MainServices/FavoriteService.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using RealStateApp.Core.Application.Interfaces.Repositories;
+using RealStateApp.Core.Application.Interfaces.Services;
+using RealStateApp.Core.Application.ViewModels.AmenityModels;
+using RealStateApp.Core.Domain.Entities;
+
+namespace RealStateApp.Core.Application.Services.MainServices
+{
+    public class AmenityService: GenericService<SaveAmenityViewModel, AmenityViewModel, Amenity>, IAmenityService
+    {
+        private readonly IRepositoryManager _repositoryManager;
+        private readonly IHttpContextAccessor _contextAccessor;
+        private readonly IMapper _mapper;
+
+        public AmenityService(IRepositoryManager repositoryManager, IHttpContextAccessor contextAccessor, IMapper mapper): base(repositoryManager.Amenity, mapper)
+        {
+            _repositoryManager = repositoryManager;
+            _contextAccessor = contextAccessor;
+            _mapper = mapper;
+        }
+    }
+}
---
+++ RealStateApp.Core.Application/Services/MainServices/PropertyService.cs
@@ -0,0 +1,53 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using RealStateApp.Core.Application.Interfaces.Repositories;
+using RealStateApp.Core.Application.Interfaces.Services;
+using RealStateApp.Core.Application.ViewModels.PropertyModels;
+using RealStateApp.Core.Domain.Entities;
+
+namespace RealStateApp.Core.Application.Services.MainServices
+{
+    public class PropertyService: GenericService<SavePropertyViewModel, PropertyViewModel, Property>, IPropertyService
+    {
+        private readonly IRepositoryManager _repositoryManager;
+        private readonly IHttpContextAccessor _contextAccessor;
+        private readonly IMapper _mapper;
+
+        public PropertyService(IRepositoryManager repositoryManager, IHttpContextAccessor contextAccessor, IMapper mapper): base(repositoryManager.Property, mapper)
+        {
+            _repositoryManager = repositoryManager;
+            _contextAccessor = contextAccessor;
+            _mapper = mapper;
+        }
+        public async Task<IEnumerable<Property>> GetAllPropertiesWithFavorites(string userId)
+        {
+
+
+            return await _repositoryManager.Property.GetAllPropertiesWithFavoritesAsync(userId);
+        }
+        public async Task<Property> GetPropertyByCode(string code)
+        {
+           return await _repositoryManager.Property.GetPropertyByCodeAsync(code);
+        }
+
+        public async Task<IEnumerable<Property>> GetPropertiesByAgentId(string agentId)
+        {
+            return await _repositoryManager.Property.GetPropertiesByAgentIdAsync(agentId);
+        }
+
+        public async Task<int> GetTotalPropertiesCount()
+        {
+            return await _repositoryManager.Property.GetTotalPropertiesCountAsync();
+        }
+        public async Task<IEnumerable<Property>> FilterProperties(string typePropertyId = "", decimal? minPrice = null, decimal? maxPrice = null, int? rooms = null, int? bathrooms = null)
+        {
+           return  await _repositoryManager.Property.FilterPropertiesAsync( typePropertyId = "",  minPrice = null, maxPrice = null,  rooms = null, bathrooms);
+        }
+        public async Task<List<Property>> GetAll()
+        {
+            return await _repositoryManager.Property.GetAllAsync();
+        }
+
+
+    }
+}
