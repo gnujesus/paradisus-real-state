2b6356693cc4b0beb4277ac9b655644bf062a952
IPolonio <Polonio.isaac@gmail.com>
2024-08-01 18:09:07 -0400

    Eso

---
+++ RealStateApp.Core.Application/Services/MainServices/PropertyAmenityService.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using RealStateApp.Core.Application.Interfaces.Repositories;
+using RealStateApp.Core.Application.Interfaces.Services;
+using RealStateApp.Core.Application.ViewModels.PropertyAmenityModels;
+using RealStateApp.Core.Domain.Entities;
+
+namespace RealStateApp.Core.Application.Services.MainServices
+{
+    public class PropertyAmenityServiceService: GenericService<SavePropertyAmenityViewModel, PropertyAmenityViewModel, PropertyAmenity>, IPropertyAmenityService
+    {
+        private readonly IRepositoryManager _repositoryManager;
+        private readonly IHttpContextAccessor _contextAccessor;
+        private readonly IMapper _mapper;
+
+        public PropertyAmenityServiceService(IRepositoryManager repositoryManager, IHttpContextAccessor contextAccessor, IMapper mapper): base(repositoryManager.PropertyAmenity, mapper)
+        {
+            _repositoryManager = repositoryManager;
+            _contextAccessor = contextAccessor;
+            _mapper = mapper;
+        }
+      
+    }
+}
