ab43fb37265d1ccec43b4bc5ae1ed2f7d578574e
Brahiam <96854137+imbrahiam@users.noreply.github.com>
2024-08-01 15:46:10 -0400

    Adding RepoManager and Services

---
+++ RealStateApp.Core.Application/Interfaces/Services/IPropertyImageService.cs
@@ -0,0 +1,11 @@
+﻿using RealStateApp.Core.Application.ViewModels.PropertyImage;
+using RealStateApp.Core.Domain.Entities;
+
+namespace RealStateApp.Core.Application.Interfaces.Services
+{
+    public interface IPropertyImageService : IGenericService<SavePropertyImageViewModel, PropertyImageViewModel, PropertyImage>
+    {
+        Task AddImagesToPropertyAsync(string propertyId, List<byte[]> images);
+        Task RemoveImageFromPropertyAsync(string propertyId, string imageId);
+    }
+}
---
+++ RealStateApp.Core.Application/ViewModels/PropertyImage/SavePropertyImageViewModel.cs
@@ -0,0 +1,17 @@
+﻿using RealStateApp.Core.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealStateApp.Core.Application.ViewModels.PropertyImage
+{
+    public class SavePropertyImageViewModel
+    {
+        public string Id { get; set; }
+        public byte[]? Image { get; set; }
+        public string PropertyId { get; set; }
+        public Property Property { get; set; }
+    }
+}
