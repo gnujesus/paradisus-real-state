738e6363eef2c5b5509eefc37d8ab74383c3cb71
Brahiam <96854137+imbrahiam@users.noreply.github.com>
2024-08-01 21:06:13 -0400

    Adding controllers

---
+++ RealStateApp/Controllers/UserController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RealStateApp.Core.Application.Interfaces.Services;
+using RealStateApp.Models;
+using System.Diagnostics;
+
+namespace RealStateApp.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly IServiceManager _serviceManager;
+
+        public UserController(IServiceManager serviceManager)
+        {
+            _serviceManager = serviceManager;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
