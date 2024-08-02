4327e11e78862387214aa080bae785dfd3b34838
gnujesus <jotamartinezd@gmail.com>
2024-08-01 21:46:50 -0400

    finished adding components to homepage

---
+++ RealStateApp/Controllers/AgentController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RealStateApp.Core.Application.ViewModels.UserModels;
+
+namespace RealStateApp.Controllers
+{
+    public class AgentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(new List<UserViewModel>());
+        }
+    }
+}
