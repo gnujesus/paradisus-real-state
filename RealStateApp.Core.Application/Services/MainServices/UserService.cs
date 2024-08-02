da1cfe2ae13de265ac0afe10316137cf0e37c090
Gilthong09 <gilthongenmanuelpalingarcia@gmail.com>
2024-08-01 23:21:16 -0400

    UserService and Login Controller

---
+++ RealStateApp.Core.Application/Services/MainServices/UserService.cs
@@ -0,0 +1,80 @@
+﻿using AutoMapper;
+using RealStateApp.Core.Application.Dtos.Account;
+using RealStateApp.Core.Application.Enums;
+using RealStateApp.Core.Application.Interfaces.Services;
+using RealStateApp.Core.Application.ViewModels.UserModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealStateApp.Core.Application.Services.MainServices
+{
+    public class UserService: IUserService
+    {
+        private readonly IAccountService _accountService;
+        private readonly IMapper _mapper;
+
+        public UserService(IAccountService accountService, IMapper mapper)
+        {
+            _accountService = accountService;
+            _mapper = mapper;
+        }
+
+        public async Task<AuthenticationResponse> LoginAsync(LoginViewModel vm)
+        {
+            AuthenticationRequest loginRequest = _mapper.Map<AuthenticationRequest>(vm);
+            AuthenticationResponse userResponse = await _accountService.AuthenticateAsync(loginRequest);
+            return userResponse;
+        }
+        public async Task SignOutAsync()
+        {
+            await _accountService.SignOutAsync();
+        }
+
+        public async Task<RegisterResponse> RegisterAsync(SaveUserViewModel vm, string origin)
+        {
+            RegisterRequest registerRequest = _mapper.Map<RegisterRequest>(vm);
+
+            if(vm.Type_User == Roles.Client.ToString())
+            {
+                return await _accountService.RegisterClientUserAsync(registerRequest, origin);
+            }
+            else
+            {
+                return await _accountService.RegisterAgentUserAsync(registerRequest);
+            }
+            
+        }
+
+        public async Task<string> ConfirmEmailAsync(string userId, string token)
+        {
+            return await _accountService.ConfirmAccountAsync(userId, token);
+        }
+
+        public async Task<ForgotPasswordResponse> ForgotPasswordAsync(ForgotPasswordViewModel vm, string origin)
+        {
+            ForgotPasswordRequest forgotRequest = _mapper.Map<ForgotPasswordRequest>(vm);
+            return await _accountService.ForgotPasswordAsync(forgotRequest, origin);
+        }
+
+        public async Task<ResetPasswordResponse> ResetPasswordAsync(ResetPasswordViewModel vm)
+        {
+            ResetPasswordRequest resetRequest = _mapper.Map<ResetPasswordRequest>(vm);
+            return await _accountService.ResetPasswordAsync(resetRequest);
+        }
+
+        public async Task<SaveUserViewModel> GetUserByUsernameAsync(string username)
+        {
+            SaveUserViewModel user = await _accountService.GetUserForUsername(username);
+            return user;
+        }
+
+        public async Task<SaveUserViewModel> UpdateUserAsync(SaveUserViewModel model)
+        {
+            SaveUserViewModel user = await _accountService.UpdateUserAsync(model);
+            return user;
+        }
+    }
+}
--- RealStateApp/Middlewares/LoginAuthorize.cs
+++ RealStateApp/Middlewares/LoginAuthorize.cs
@@ -1,6 +1,28 @@
-﻿namespace RealStateApp.Middlewares
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using RealStateApp.Controllers;
+
+namespace RealStateApp.Middlewares
 {
-    public class LoginAuthorize
+    public class LoginAuthorize : IAsyncActionFilter
     {
+        private readonly ValidateUserSession _userSession;
+
+        public LoginAuthorize(ValidateUserSession userSession)
+        {
+            _userSession = userSession;
+        }
+
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            if (_userSession.HasUser())
+            {
+                var controller = (LoginController)context.Controller;
+                context.Result = controller.RedirectToAction("index", "home");
+            }
+            else
+            {
+                await next();
+            }
+        }
     }
 }
