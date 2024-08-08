70e4dcb72fe41c4d7b5f28c172a188c5cd6fcdb9
gnujesus <jotamartinezd@gmail.com>
2024-08-07 22:48:22 -0400

    finished crud except properties

--- RealStateApp/Controllers/AgentController.cs
+++ RealStateApp/Controllers/AgentController.cs
@@ -19,7 +19,7 @@ namespace RealStateApp.Controllers
         }
         public async Task<IActionResult> Index()
         {
-            var agents = await _accountService.GetAgentsAsync();
+            var agents = await _serviceManager.user.GetAllAgentsAsync();
             return View(agents);
         }
 
@@ -29,7 +29,7 @@ namespace RealStateApp.Controllers
 
             AuthenticationResponse user = HttpContext.Session.Get<AuthenticationResponse>("user") ?? new();
 
-            if (user.Roles[0] == "Guest")
+            if (user.Roles.Count() == 0)
             {
                 return RedirectToRoute(new { Controller = "Login", Action = "Index" });
             }
